5412f9107ffd6f5a8f62208867d3ba962c9a5874
AghayevEmin <emin.agayev.99@gmail.com>
2021-09-04 11:29:36 +0200

    script added

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash 
+
+#Compress the folder with foldernames + date and take backup variable
+filename="backup_`date +%d`_`date +%m`_`date +%Y`.tar";
+
+#find .gz files with find and create compressed file using tar
+find /var/log/ -name "*.gz" -exec tar cvpzf /home/emin/Projects/backup/$filename.tar {} \;
+
+#move to backup folder
+cd /home/emin/Projects/backup/
+
+# List the content
+ls
+
+# List the disk usage
+du -sh
+
